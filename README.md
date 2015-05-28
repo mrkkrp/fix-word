c421faf6c970fc31402ddb1151f5d713e7b67783
mrkkrp <markkarpov@opmbx.org>
2015-05-28 14:26:01 +0600

    initial commit

---
+++ README.md
@@ -0,0 +1,110 @@
+# Fix Word
+
+This is a package that allows to transform words intelligently. Fix Word
+provides function `fw/fix-word` that lifts functions that do string
+transformation into commands with interesting behavior. There are also some
+built-in command based on `fw/fix-word`, see below.
+
+## Installation
+
+Use your package manager: <kbd>M-x package-install fix-word</kbd>. It's in
+MELPA.
+
+## API Description
+
+```
+fw/fix-word fnc
+```
+
+Lift function `fnc` into command that operates on words and regions.
+
+The following behaviors are implemented:
+
+If the point is placed outside of a word, apply `FNC` to previous word. If
+the command is invoked repeatedly, every its invocation transforms one more
+word moving from right to left. For example (upcasing, ^ shows position of
+point/cursor):
+
+```
+The quick brolwn fox jumps over the lazy dog.^
+The quick brown fox jumps over the lazy DOG.^
+The quick brown fox jumps over the LAZY DOG.^
+The quick brown fox jumps over THE LAZY DOG.^
+```
+
+The point doesn't move, this allows user to fix recently entered
+words and continue typing.
+
+If the point is placed inside any part of a word, the whole word is
+transformed. The point is moved to first character of the next word. This
+allows to transform words repeatedly pressing dedicated key binding.
+
+```
+^The quick brown fox jumps over the lazy dog.
+THE ^quick brown fox jumps over the lazy dog.
+THE QUICK ^brown fox jumps over the lazy dog.
+THE QUICK BROWN ^fox jumps over the lazy dog.
+```
+
+If there is an active region, all words in that region are transformed.
+
+Use `fw/fix-word`` to create new commands like this:
+
+
+```emacs-lisp
+(defun command-name ()
+  \"Description of the command.\"
+  (interactive)
+  (fw/fix-word #'upcase))
+```
+
+There is also a macro that defines such commands for you:
+`fw/define-command`.
+
+## Built-in Commands
+
+So, here I describe built-in commands based on `fw/fix-word`.
+
+Default commands to upcase/downcase/capitalize stuff are not very convenient
+to say the least, for the following reasons:
+
+1. There are three different commands to upcase thing, for example. User
+   needs to remember three commands, their key bindings, and when to use
+   each of them. There should be one command per action: one for upcasing,
+   one for downcasing, and one for capitalizing.
+
+2. Commands on regions doesn't have dedicated key bindings and what's even
+   worse: they are disabled by default!
+
+3. Commands like `upcase-word` depend on position of pointer inside word,
+   now tell me what's expected result of upcase command here: "fo^o"? I bet
+   you want "FOO", not "foO". The same applies to capitalization, etc. These
+   commands should work on entire word no matter what.
+
+4. You need to use arguments for commands like `upcase-word` to make them
+   correct words that you've just written. What's even worse, you can fix
+   only one last word this way. What if you want to fix a couple of them?
+
+Here are commands that are trying to fix all these flaws:
+
+* `fw/upcase`
+* `fw/downcase`
+* `fw/capitalize`
+
+I propose replacing of old functions with these, you can do it this way:
+
+```emacs-lisp
+(global-set-key (kbd "M-u") #'fw/upcase)
+(global-set-key (kbd "M-l") #'fw/downcase)
+(global-set-key (kbd "M-c") #'fw/capitalize)
+```
+
+There are many ways `fw/fix-word` can be used. For example, if you have a
+function `translate` that translates some text, you can get a command that
+can translate words. It will work in all those modes described here.
+
+## License
+
+Copyright © 2015 Mark Karpov
+
+Distributed under GNU GPL, version 3.
---
+++ fix-word.el
@@ -0,0 +1,152 @@
+;;; fix-word.el --- Convenient word transformation -*- lexical-binding: t; -*-
+;;;
+;;; Copyright © 2015 Mark Karpov <markkarpov@openmailbox.org>
+;;;
+;;; Author: Mark Karpov <markkarpov@openmailbox.org>
+;;; URL: https://github.com/mrkkrp/fix-word
+;;; Version: 0.1.0
+;;; Package-Requires: ((emacs "24.1") (cl-lib "0.5"))
+;;; Keywords: word, convenience
+;;;
+;;; This file is not part of GNU Emacs.
+;;;
+;;; This program is free software: you can redistribute it and/or modify it
+;;; under the terms of the GNU General Public License as published by the
+;;; Free Software Foundation, either version 3 of the License, or (at your
+;;; option) any later version.
+;;;
+;;; This program is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+;;; Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License along
+;;; with this program. If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;; This package provides means of lifting functions that transform strings
+;;; into commands that have different behavior depending on active region,
+;;; position of point, etc.
+
+;;; Code:
+
+;;;###autoload
+(defun fw/fix-word (fnc)
+  "Lift function FNC into command that operates on words and regions.
+
+The following behaviors are implemented:
+
+If the point is placed outside of a word, apply FNC to previous
+word.  If the command is invoked repeatedly, every its invocation
+transforms one more word moving from right to left.  For
+example (upcasing, ^ shows position of point/cursor):
+
+  The quick brolwn fox jumps over the lazy dog.^
+  The quick brown fox jumps over the lazy DOG.^
+  The quick brown fox jumps over the LAZY DOG.^
+  The quick brown fox jumps over THE LAZY DOG.^
+
+The point doesn't move, this allows user to fix recently entered
+words and continue typing.
+
+If the point is placed inside any part of a word, the whole word
+is transformed.  The point is moved to first character of the
+next word.  This allows to transform words repeatedly pressing
+dedicated key binding.
+
+  ^The quick brown fox jumps over the lazy dog.
+  THE ^quick brown fox jumps over the lazy dog.
+  THE QUICK ^brown fox jumps over the lazy dog.
+  THE QUICK BROWN ^fox jumps over the lazy dog.
+
+If there is an active region, all words in that region are
+transformed.
+
+Use `fw/fix-word' to create new commands like this:
+
+\(defun command-name ()
+  \"Description of the command.\"
+  (interactive)
+  (fw/fix-word #'upcase))
+
+There is also a macro that defines such commands for you:
+`fw/define-command'."
+  (funcall
+   (if (region-active-p)
+       #'fw/-fix-region
+     (if (looking-at "\\w+\\>")
+         #'fw/-fix-and-move
+       #'fw/-fix-quickly))
+   fnc))
+
+(defun fw/-fix-region (fnc)
+  "Transform active region with function FNC."
+  (let* ((from (point))
+         (to   (mark))
+         (str  (buffer-substring-no-properties from to)))
+    (delete-region from to)
+    (insert (funcall fnc str))
+    (goto-char from)))
+
+(defun fw/-fix-and-move (fnc)
+  "Transform current word with function FNC and move to the next word."
+  (fw/-transform-word fnc)
+  (forward-word 2)
+  (backward-word))
+
+(defvar fw/-quick-fix-times 1
+  "How many times `fw/-fix-quickly' has been invoked consequently.")
+
+(defun fw/-fix-quickly (fnc)
+  "Transform previous word with function FNC.
+If this function is invoked repeatedly, transform more words
+moving from right to left."
+  (interactive)
+  (let* ((origin (point))
+         (i (if (eq last-command this-command)
+                (setf fw/-quick-fix-times
+                      (1+ fw/-quick-fix-times))
+              (setf fw/-quick-fix-times 1))))
+    (backward-word i)
+    (fw/-transform-word fnc)
+    (goto-char origin)))
+
+(defun fw/-transform-word (fnc)
+  "Transform word at point with function FNC."
+  (let* ((origin (point))
+         (from   (re-search-backward "\\<\\|\\W" nil t))
+         (to     (progn
+                   (goto-char origin)
+                   (re-search-forward  "\\>" nil t))))
+    (when (and from to)
+      (let ((str (buffer-substring-no-properties from to)))
+        (delete-region from to)
+        (insert (funcall fnc str))
+        (goto-char origin)))))
+
+;;;###autoload
+(defmacro fw/define-command (name fnc doc)
+  "Define `fw/fix-word'-based command named NAME.
+FNC is the processing function and DOC is documentation string."
+  (declare (indent defun))
+  `(defun ,name ()
+     (interactive)
+     ,(concat doc "\n\nSee function `fw/fix-word' for more information.")
+     (fw/fix-word ,fnc)))
+
+;; Here are some default commands implemented with `fw/fix-word'.
+
+;;;###autoload
+(fw/define-command fw/upcase #'upcase
+  "Upcase word intelligently.")
+;;;###autoload
+(fw/define-command fw/downcase #'downcase
+  "Downcase word intelligently.")
+;;;###autoload
+(fw/define-command fw/capitalize #'capitalize
+  "Capitalize word intelligently.")
+
+(provide 'fix-word)
+
+;;; fix-word.el ends here
