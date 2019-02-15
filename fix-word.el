b3b3a3c8e33e425f9a8d0ec653adb6897c8efc03
mrkkrp <markkarpov92@gmail.com>
2019-02-15 21:38:35 +0100

    Version bump (0.2.0)
    [skip ci]

--- fix-word.el
+++ fix-word.el
@@ -4,7 +4,7 @@
 ;;
 ;; Author: Mark Karpov <markkarpov92@gmail.com>
 ;; URL: https://github.com/mrkkrp/fix-word
-;; Version: 0.1.2
+;; Version: 0.2.0
 ;; Package-Requires: ((emacs "24.1") (cl-lib "0.5"))
 ;; Keywords: word, convenience
 ;;
